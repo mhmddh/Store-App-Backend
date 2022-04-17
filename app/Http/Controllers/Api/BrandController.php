b8b982583197648597a429b864098cf5b32d1914
mhmddh <mohamad.hsen.daher@gmail.com>
2022-04-17 18:35:18 +0300

    Update BrandController.php

--- app/Http/Controllers/Api/BrandController.php
+++ app/Http/Controllers/Api/BrandController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Brand;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
 
 class BrandController extends Controller
 {
