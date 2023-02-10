fb2a825f26a0980eac5d41779eb6531e3dc8ae55
mhmddh <mohamad.hsen.daher@gmail.com>
2023-02-11 00:33:10 +0200

    Fixes

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -16,24 +16,28 @@ Login API
      * */
     public function login(Request $request)
     {
-        //User check
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-            $id = Auth::user()->id;
-            $user = User::find($id);
-            //Setting login response
-            $success['id'] = $user->id;
-            $success['token'] = $user->createToken('auth-token')->plainTextToken;
-            $success['name'] = $user->name;
+        try {
+            //User check
+            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+                $id = Auth::user()->id;
+                $user = User::find($id);
+                //Setting login response
+                $success['id'] = $user->id;
+                $success['token'] = $user->createToken('auth-token')->plainTextToken;
+                $success['name'] = $user->name;
 
-            return response()->json([
-                'status' => 'success',
-                'data' => $success
-            ]);
-        } else {
-            return response()->json([
-                'status' => 'error',
-                'data' => 'Unauthorized Access'
-            ]);
+                return response()->json([
+                    'status' => 'success',
+                    'data' => $success
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 'error',
+                    'data' => 'Unauthorized Access'
+                ]);
+            }
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()]);
         }
     }
 }
